d2edbd96cc1d4eca01fa6b44d69fefe10119038a
hubajoo <joohuba@gmail.com>
2024-11-20 20:43:10 +0100

    Added game

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+//const fs = require('fs');
+const { Pool } = require('pg');
+//const path = require('path');
 const app = express();
 const port = 8090;
 
 console.log(`Server starting on ${port}`);
 
-app.all('/', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  console.log(req.url);
-  res.status(404).send('request');
-});
-
-app.get('/api/leaderboard', (req, res) => {
+app.get('/api/leaderboard2', (req, res) => {
   const filePath = path.join(__dirname, 'GameFiles', 'leaderboard.txt');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -23,6 +18,15 @@ app.get('/api/leaderboard', (req, res) => {
     console.log(`File sent to ${req.ip}`);
     res.status(200).type('text/plain').send(data);
   });
+});
+app.get('/leaderboard', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM leaderboard');
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error('Error querying database:', err);
+    res.status(500).send('Error loading leaderboard');
+  }
 });
 
 app.use((req, res) => {
